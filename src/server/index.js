098c0113e60f3ee8e5453288823b4e6146937878
Keaton <keatondkirkpatrick@gmail.com>
2023-02-17 14:56:41 -0800

    updated server to prod db

--- src/server/index.js
+++ src/server/index.js
@@ -8,7 +8,7 @@ const upload = multer({dest: "uploads/"})
 
 const FormData = require('form-data')
 const fs = require('fs');
-const client = require("./db")
+const client = require("./prod_db")
 const cookieParser = require("cookie-parser");
 require('dotenv').config();
 const axios = require('axios')
